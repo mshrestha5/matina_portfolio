89f9df5179745de591a21bb080f8f292347d13e1
Matina Shrestha <104951387+mshrestha5@users.noreply.github.com>
2023-04-06 13:05:16 -0500

    3rd commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,12 +7,12 @@ import { AiFillYoutube } from "react-icons/ai";
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">
+      <a href="/" className="footer__logo">
         Portfolio
       </a>
       <ul className="permalinks">
         <li>
-          <a href="#">Home</a>
+          <a href="/">Home</a>
         </li>
         <li>
           <a href="#about">About</a>
